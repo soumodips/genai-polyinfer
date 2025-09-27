0dbba3caf190c3c862b8f3a742a1521a6ecba703
soumodips <smdpsinha@gmail.com>
2025-09-28 04:09:10 +0530

    tsup config updated

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,33 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  outExtension({ format }) {
-    return {
-      js: format === 'cjs' ? '.cjs.js' : '.esm.js'
-    };
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['cjs'],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    target: 'node14',
+    outExtension({ format }) {
+      return {
+        js: format === 'cjs' ? '.cjs.js' : '.esm.js'
+      };
+    }
+  },
+  {
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: false, // Only generate once
+    splitting: false,
+    sourcemap: true,
+    clean: false,
+    target: 'es2020',
+    external: ['node-fetch'],
+    outExtension({ format }) {
+      return {
+        js: format === 'cjs' ? '.cjs.js' : '.esm.js'
+      };
+    }
   }
-});
+]);
